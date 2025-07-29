593786a3deae8e6b136013637140a606300d3c08
Harshan07-web <bharshan17@gmail.com>
2025-07-29 16:10:56 +0530

    Remove tracked files that are now in .gitignore

--- loaders/pdf_loader.py
+++
@@ -1,9 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
-
-def load_sample_file(path , chunk_size=1000,chunk_overlap=200):
-    loader = PyPDFLoader(path)
-    pages = loader.load()
-    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
-    return splitter.split_documents(pages)
-
