3313ac3b75dde9d117f9c8e4d91a521966494ed6
Harshan <bharshan17@gmail.com>
2025-12-12 16:38:13 +0530

    basic changes

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -6,6 +6,6 @@ from langchain_community.vectorstores import FAISS
 def create_load_vectorstore(docs, index_path="faiss_index"):
     embeddings = GoogleGenerativeAIEmbeddings(
         model="models/embedding-001",
-        google_api_key="AIzaSyBnXqsczmdQ5uRwpOabS-m4vNJ9ue_lySU"
+        google_api_key=""
     )
     return FAISS.from_documents(docs, embeddings)
